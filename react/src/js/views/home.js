237c2c7600eb05417adeae53455d9a79a3cd9bf0
MageLnk <mage.link.zhezard@gmail.com>
2019-07-24 12:32:13 -0400

    Estructura del registro lista

--- react/src/js/views/home.js
+++ react/src/js/views/home.js
@@ -6,7 +6,7 @@ export class Home extends React.Component {
 	render() {
 		return (
 			<div className="text-center mt-5">
-				<h1>Ola k ase - Home</h1>
+				<h1>Ola k ase - CAMPAÑAS ACÁ</h1>
 			</div>
 		);
 	}
--- react/src/js/views/register.js
+++ react/src/js/views/register.js
@@ -20,66 +20,68 @@ export class Registro extends React.Component {
 							<form>
 								<div className="form-row">
 									<div className="form-group col-md-6">
-										<label htmlFor="inputEmail4">Email</label>
+										<label htmlFor="inputnombre">Nombre</label>
 										<input
-											type="email"
+											type="text"
 											className="form-control"
-											id="inputEmail4"
-											placeholder="Email"
+											id="inputnombre"
+											placeholder="Nombre"
 										/>
 									</div>
 									<div className="form-group col-md-6">
-										<label htmlFor="inputPassword4">Password</label>
+										<label htmlFor="inputnick">Nombre de usuario</label>
+										<input
+											type="text"
+											className="form-control"
+											id="inputnick"
+											placeholder="Nombre de usuario"
+										/>
+									</div>
+									<div className="form-group col-md-6">
+										<label htmlFor="inputPassword1">Password</label>
 										<input
 											type="password"
 											className="form-control"
-											id="inputPassword4"
+											id="inputPassword1"
 											placeholder="Password"
+										/>
+									</div>
+									<div className="form-group col-md-6">
+										<label htmlFor="inputPassword2">Confirmación Password</label>
+										<input
+											type="password"
+											className="form-control"
+											id="inputPassword2"
+											placeholder="Password"
+										/>
+									</div>
+									<div className="form-group col-md-6">
+										<label htmlFor="inputEmail1">Email</label>
+										<input
+											type="email"
+											className="form-control"
+											id="inputEmail1"
+											placeholder="Ingrese email"
+										/>
+									</div>
+									<div className="form-group col-md-6">
+										<label htmlFor="inputEmail2">Confirmación Email</label>
+										<input
+											type="email"
+											className="form-control"
+											id="inputEmail2"
+											placeholder="Ingrese email"
 										/>
 									</div>
 								</div>
 								<div className="form-group">
-									<label htmlFor="inputAddress">Address</label>
+									<label htmlFor="inputrut">Ingrese su rut</label>
 									<input
-										type="text"
+										type="number"
 										className="form-control"
-										id="inputAddress"
-										placeholder="1234 Main St"
+										id="inputrut"
+										placeholder="12345678-9"
 									/>
-								</div>
-								<div className="form-group">
-									<label htmlFor="inputAddress2">Address 2</label>
-									<input
-										type="text"
-										className="form-control"
-										id="inputAddress2"
-										placeholder="Apartment, studio, or floor"
-									/>
-								</div>
-								<div className="form-row">
-									<div className="form-group col-md-6">
-										<label htmlFor="inputCity">City</label>
-										<input type="text" className="form-control" id="inputCity" />
-									</div>
-									<div className="form-group col-md-4">
-										<label htmlFor="inputState">State</label>
-										<select id="inputState" className="form-control">
-											<option selected>Choose...</option>
-											<option>...</option>
-										</select>
-									</div>
-									<div className="form-group col-md-2">
-										<label htmlFor="inputZip">Zip</label>
-										<input type="text" className="form-control" id="inputZip" />
-									</div>
-								</div>
-								<div className="form-group">
-									<div className="form-check">
-										<input className="form-check-input" type="checkbox" id="gridCheck" />
-										<label className="form-check-label" htmlFor="gridCheck">
-											Check me out
-										</label>
-									</div>
 								</div>
 								<Link to="/login">
 									<button type="submit" className="btn btn-primary">
