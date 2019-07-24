6a079b18574010a2d4c69a4f450c1f0f63814b14
Jonathan <joncomas@outlook.com>
2019-07-24 19:10:39 +0000

    2407191510

--- api/api/models.py
+++ api/api/models.py
@@ -7,11 +7,27 @@ from django.db import models
 """
 Define he Contact Entity into your applcation model
 """
-class Contact(models.Model):
+class User(models.Model):
     first_name = models.CharField(max_length=50, default='')
     last_name = models.CharField(max_length=50, default='')
     phone_number = models.CharField(max_length=15, default='')
     email = models.CharField(max_length=150, default='')
+
+class Person(models.Model):
+    fk_user = models.ForeignKey(User)
+    name = models.CharField(max_length=50, default='')
+    rut_person = models.IntegerField(max_length=20, default='')
+    birth_date = models.DateField(input_formats=settings.DATE_INPUT_FORMATS)
+
+class Campaign(models.Model):
+    fk_user = models.ForeignKey(User)
+    name_camp = models.CharField(max_length=50, default='')
+    search_target = models.CharField(max_length=50, default='')
+    item_to_search = models.CharField(max_length=150, default='')
+    start_date = models.DateField(input_formats=settins.DATE_INPUT_FORMATS)
+
+
+
 
 """
 The ContactSerializer is where you will specify what properties
--- api/example_project/settings.py
+++ api/example_project/settings.py
@@ -113,7 +113,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'example_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
