f3d2288bf03689d269a2b487757b6f7244858851
Jonathan <joncomas@outlook.com>
2019-07-24 15:58:45 +0000

    authenticator

--- api/api/views.py
+++ api/api/views.py
@@ -2,12 +2,15 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from api.models import Contact, ContactSerializer
+from rest_framework.permissions import IsAuthenticated
 
 """
 The ContactsView will contain the logic on how to:
  GET, POST, PUT or delete the contacts
 """
 class ContactsView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, contact_id=None):
 
         if contact_id is not None:
