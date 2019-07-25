e71fa763d06a13e1409d3d7d2614f7ab5dbdc117
MageLnk <mage.link.zhezard@gmail.com>
2019-07-25 17:13:48 -0400

    Tengo el token!

--- react/src/js/store/flux.js
+++ react/src/js/store/flux.js
@@ -12,7 +12,12 @@ const getState = ({ getStore, setStore }) => {
 				password: "",
 				email: "",
 				rut: ""
-			}
+			},
+			inputsLogin: {
+				username: "",
+				password: ""
+			},
+			InputsToken: {}
 		},
 		actions: {
 			obtenerDataCamp: evento => {
@@ -30,8 +35,14 @@ const getState = ({ getStore, setStore }) => {
 				oldStore[name] = evento.target.value;
 				setStore({ inputsRegistro: oldStore });
 			},
+			obtenerDatosLogin: evento => {
+				const store = getStore();
+				const name = evento.target.name;
+				let oldStore = store.inputsLogin;
+				oldStore[name] = evento.target.value;
+				setStore({ inputsLogin: oldStore });
+			},
 			registroUsuario: contacto => {
-				console.log(contacto);
 				fetch("https://3000-a9e90353-6f2d-479c-9912-869cf4ee8d41.ws-us0.gitpod.io/api/register/", {
 					method: "Post",
 					body: JSON.stringify(contacto),
@@ -41,6 +52,23 @@ const getState = ({ getStore, setStore }) => {
 				})
 					.then(resp => resp.json())
 					.then(resp => {
+						console.log(resp);
+					});
+			},
+			// Abajo me logeo e intentaré obtener el token
+			loginUsuario: contacto => {
+				fetch("https://3000-a9e90353-6f2d-479c-9912-869cf4ee8d41.ws-us0.gitpod.io/api/token/", {
+					method: "Post",
+					body: JSON.stringify(contacto),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(resp => resp.json())
+					.then(resp => {
+						setStore({
+							InputsToken: resp
+						});
 						console.log(resp);
 					});
 			}
