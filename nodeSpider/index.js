a6075d43cac835b7ed4a8f98ee01daa8c443d52d
Jonathan <joncomas@outlook.com>
2019-08-06 23:48:21 +0000

    080620191948

--- nodeSpider/index.js
+++ nodeSpider/index.js
@@ -1,6 +1,9 @@
 const save = require('save-file');
 const search = require('./search');
 const getter = require('request-promise');
+const rp = require('request-promise');
+const url = 'https://www.portalinmobiliario.com';
+const $ = require('cheerio');
 fs = require('fs');
 
 /*
@@ -15,13 +18,23 @@ search.getListURL().then((res) => {
 });
 */
 
-
-
-
 fs.readFile(__dirname + '/data.json', (err, data) => {
-  if (err) console.log('aqui ', err);
-  let obj = JSON.parse(data);
-  dataStringified = data.toString();
-  console.log('AQUUIIII!!!!!', obj.lista[0])
+    if (err) console.log('aqui ', err);
+    let obj = JSON.parse(data);
+    dataStringified = data.toString();
+    let publication = obj.lista[0]
+    fullUrl = url + publication
+    const target = url + publication;
+
+
+    rp(target)
+        .then(function(html) {
+            console.log($('.prj-phones-item', html).text());
+            //console.log($('.bday', html).text());
+        })
+        .catch(function(err) {
+            //handle error
+        });
 });
+
 
--- nodeSpider/phoneGrabber.js
+++ nodeSpider/phoneGrabber.js
@@ -1,28 +1,12 @@
-
 const rp = require('request-promise');
 const $ = require('cheerio');
+const url = 'https://en.wikipedia.org/wiki/George_Washington';
 
-let getPhoneListURL = async () => {
-    const maxSearchPages = 3;
-    const arrUrl = [];
-
-    for (let page = 0; page < maxSearchPages; page++) {
-        const url = 'https://www.portalinmobiliario.com/venta/casa/lo-barnechea-metropolitana?ca=3&pd=500000&ph=800000&ts=1&dd=4&mn=3&or=&sf=1&sp=0&at=0&pg=' + (page + 1);
-        const html = await rp(url);
-
-        const lista = $('.product-item', html);
-        if (lista !== undefined && lista.length > 0) {
-            for (let i = 0; i < lista.length; i++) {
-                arrUrl.push($('.product-item-summary a', lista[i])[0].attribs.href);
-            }
-        } else {
-            console.log('NO se encontraron listas')
-        }
-    }
-
-    return arrUrl;
-};
-
-module.exports = {
-    getListURL
-}
+rp(url)
+  .then(function(html) {
+    console.log($('.firstHeading', html).text());
+    console.log($('.bday', html).text());
+  })
+  .catch(function(err) {
+    //handle error
+  });
