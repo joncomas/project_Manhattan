7ff1ca97d426d03b2d24c9e2b2d2c444cf44c3e7
Jonathan <joncomas@outlook.com>
2019-08-06 22:42:14 +0000

    080620191842

--- nodeSpider/index.js
+++ nodeSpider/index.js
@@ -1,5 +1,27 @@
+const save = require('save-file');
 const search = require('./search');
+const getter = require('request-promise');
+fs = require('fs');
 
+/*
 search.getListURL().then((res) => {
-    console.log('resp', res.length);
+
+    save(JSON.stringify({
+        lista: res
+    }), 'data.json').then((file) => {
+        console.log('resp', res.length);
+    })
+
 });
+*/
+
+
+
+
+fs.readFile(__dirname + '/data.json', (err, data) => {
+  if (err) console.log('aqui ', err);
+  let obj = JSON.parse(data);
+  dataStringified = data.toString();
+  console.log('AQUUIIII!!!!!', obj.lista[0])
+});
+
