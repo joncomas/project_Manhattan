6e641479c78c66d82685b7b88f68a02066f512ec
Jonathan <joncomas@outlook.com>
2019-08-08 20:11:12 +0000

    phonesToDB

--- nodeSpider/index.js
+++ nodeSpider/index.js
@@ -2,15 +2,19 @@
 const sqlite3 = require('sqlite3').verbose();
 const save = require('save-file');
 //Imports from folder
-const search = require('./search');
-const dbWritter = require('./dbWritter');
+//const search = require('./search');
+//const dbWritter = require('./dbWritter');
+const phoneGrabber = require('./phoneGrabber')
 //System required
 const rp = require('request-promise');
 const $ = require('cheerio');
 const fs = require('fs');
 
-console.log(search);
-console.log(dbWritter);
+//console.log(search);
+//console.log(dbWritter);
+phoneGrabber.getAll((resp) => {
+    console.log('Aqui estra completo', resp)
+});
 
 
 
--- nodeSpider/phoneGrabber.js
+++ nodeSpider/phoneGrabber.js
@@ -14,44 +14,59 @@ search.getListURL().then((res) => {
     save(JSON.stringify({
         lista: res
     }), 'data.json').then((file) => {
-        console.log('resp', res.length);
+        //console.log('resp', res.length);
     })
 
 });
 
 // Here we read from the data.json created to extract the info needed from each one
-fs.readFile(__dirname + '/data.json', (err, data) => {
 
-    concatUrlsList = [];
-    phonesList = {
-        phoneNumber: 0,
-        url: ''
-    };
-    totalList = [];
+const getAll = (callback) => {
 
-//Here we parse the data from the file.json
-    if (err) console.log('aqui ', err);
-    let obj = JSON.parse(data);
-    //This for loop joins the urls contained in the lista and the ppal url.
-    for (i = 0; i < obj.lista.length; i++) {
-        let publication = obj.lista[i]
-        const target = url + publication;
-        concatUrlsList.push(target)
-    }
-    // This for loop goes over each concatenated url
-    for (j = 0; j < concatUrlsList.length; j++) {
-        rp(concatUrlsList[j])
-            .then(function(html) {
-                //Here it looks for the class holding the phone number
-                goldMine = $('.prj-phones-item', html).text()
-                // Here the numbers gotten get cleaned
-                catchedPhoneNumber = goldMine.replace(/\s+/g, '');
+    fs.readFile(__dirname + '/data.json', async (err, data) => {
 
-            })
-            .catch(function(err) {
-                //handle error
-        });
-    }
-});
+        concatUrlsList = [];
+        catchedPhoneNumber = [];
+
+        //Here we parse the data from the file.json
+        if (err) console.log('aqui ', err);
+        let obj = JSON.parse(data);
+        //This for loop joins the urls contained in the lista and the ppal url.
+        for (i = 0; i < obj.lista.length; i++) {
+            let publication = obj.lista[i]
+            const target = url + publication;
+            concatUrlsList.push(target)
+        }
+        // This for loop goes over each concatenated url
+        for (j = 0; j < concatUrlsList.length; j++) {
+            const result = await rp(concatUrlsList[j]);
+
+            goldMine = $('.prj-phones-item', result).text().trim().split(/\n/g);
+
+            goldMine.forEach((itemClean) => {
+                if (itemClean.trim() !== '') {
+                    catchedPhoneNumber.push(itemClean.trim().replace(/\s+/g, ''));
+                }
+            });
+
+            /*
+
+            // Here the numbers gotten get cleaned
+            trimmed = goldMine.trim()
+            if (trimmed != "") {
+                //catchedPhoneNumber = trimmed.replace(/\s+/g, '');
+                catchedPhoneNumber.push(trimmed)
+
+            }
+            */
+        }
+
+        callback(catchedPhoneNumber);
+    });
+
+}
 
 
+module.exports = {
+    getAll
+}
